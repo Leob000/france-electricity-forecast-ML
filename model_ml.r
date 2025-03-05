5bbb771b89cb0b2b2dd4d59d3cb104469b75cef3
Leo Burgund <leo.burgund@gmail.com>
2025-03-05 19:43:05 +0100

    implémentation RF

--- model_ml.r
+++ model_ml.r
@@ -25,42 +25,56 @@ df_full$Net_demand.7 <- (df_full$Net_demand.7 - Net_demand_mean) / Net_demand_sd
 # Création des jeux de données train, val, test; et un jeu qui combine train et val
 idx_train <- which(df_full$Date <= "2021-09-01")
 idx_val <- which((df_full$Date > "2021-09-01") & (df_full$Date <= "2022-09-01"))
+idx_train_val <- which(df_full$Date <= "2022-09-01")
 idx_test <- which(df_full$Date > "2022-09-01")
 df_train <- df_full[idx_train, ]
 df_val <- df_full[idx_val, ]
+df_train_val <- df_full[idx_train_val, ]
 df_test <- df_full[idx_test, ]
 
 ### Models
 target_col <- "Net_demand"
 features_col <- setdiff(names(df_full), target_col)
 features_col <- setdiff(features_col, "Date")
+# features_col <- setdiff(features_col, "lundi_vendredi")
 features_col
 
-# Train a basic random forest model
 library(randomForest)
-rf_model <- randomForest(as.formula(paste(target_col, "~ .")), data = df_train[, c(features_col, target_col)], importance = TRUE)
-
-# Print the model summary
-print(rf_model)
-
-# Predict on the validation set
-predictions <- (predict(rf_model, df_val[, features_col]) * Net_demand_sd) + Net_demand_mean
-ground_truth <- (df_val$Net_demand * Net_demand_sd) + Net_demand_mean
-
-# Calculate and print the RMSE
-rmse <- sqrt(mean((ground_truth - predictions)^2))
-print(paste("RMSE on validation set:", rmse))
-
-err_pinball <- pinball_loss(ground_truth, predictions, 0.8)
-print(paste("Pinball on validation set:", err_pinball))
-
-# Get the importance of variables
-importance <- importance(rf_model)
-print(importance)
-
-# Plot the importance of variables
-varImpPlot(rf_model)
-# Plot predictions and ground truth as lines
-plot(df_val$Date, ground_truth, type = "l", col = "blue", lwd = 2, ylab = "Net Demand", xlab = "Date", main = "Predictions vs Ground Truth")
-lines(df_val$Date, predictions, col = "red", lwd = 2)
-legend("topright", legend = c("Ground Truth", "Predictions"), col = c("blue", "red"), lwd = 2)
+# RandomForest basique
+# TODO FAire OOB error au lieu de val
+VALIDATION <- FALSE
+CHEAT <- TRUE
+if (VALIDATION) { # Sur validation 2021
+    rf_model <- randomForest(as.formula(paste(target_col, "~ .")), data = df_train[, c(features_col, target_col)], importance = TRUE)
+    print(rf_model)
+    predictions <- (predict(rf_model, df_val[, features_col]) * Net_demand_sd) + Net_demand_mean
+    ground_truth <- (df_val$Net_demand * Net_demand_sd) + Net_demand_mean
+    err_pinball <- pinball_loss(ground_truth, predictions, 0.8)
+    print(paste("Pinball on validation set:", err_pinball))
+    err_rmse <- sqrt(mean((ground_truth - predictions)^2))
+    print(paste("RMSE on validation set:", err_rmse))
+    plot(df_val$Date, ground_truth, type = "l", col = "blue", lwd = 2, ylab = "Net Demand", xlab = "Date", main = "Predictions vs Ground Truth (Validation 2021)")
+    lines(df_val$Date, predictions, col = "red", lwd = 2)
+    legend("topright", legend = c("Ground Truth", "Predictions"), col = c("blue", "red"), lwd = 2)
+    importance <- importance(rf_model)
+    varImpPlot(rf_model)
+} else { # Complet, prédictions 2022
+    rf_model <- randomForest(as.formula(paste(target_col, "~ .")), data = df_train_val[, c(features_col, target_col)], importance = TRUE)
+    print(rf_model)
+    predictions <- (predict(rf_model, df_test[, features_col]) * Net_demand_sd) + Net_demand_mean
+    if (CHEAT) {
+        ground_truth <- read.csv("Data/test_better.csv")$Net_demand
+        err_pinball <- pinball_loss(ground_truth, predictions, 0.8)
+        print(paste("Pinball on validation set:", err_pinball))
+        err_rmse <- sqrt(mean((ground_truth - predictions)^2))
+        print(paste("RMSE on validation set:", err_rmse))
+        plot(df_test$Date, ground_truth, type = "l", col = "blue", lwd = 2, ylab = "Net Demand", xlab = "Date", main = "Predictions vs Ground Truth (Cheat 2022)")
+        lines(df_test$Date, predictions, col = "red", lwd = 2)
+        legend("topright", legend = c("Ground Truth", "Predictions"), col = c("blue", "red"), lwd = 2)
+    } else {
+        plot(df_test$Date, predictions, type = "l", col = "red", lwd = 2, ylab = "Net Demand", xlab = "Date", main = "Predictions vs Ground Truth (Preds 2022)")
+        legend("topright", legend = c("Predictions"), col = c("red"), lwd = 2)
+    }
+    importance <- importance(rf_model)
+    varImpPlot(rf_model)
+}
