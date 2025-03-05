eeccca79c55e2d752442a3280283be8b12140c21
Leo Burgund <leo.burgund@gmail.com>
2025-03-05 12:38:59 +0100

    Enable graph plotting for Nebulosity and Nebulosity_weighted; add weighted ratios and update correlation matrix visualization

--- explo.r
+++ explo.r
@@ -4,13 +4,13 @@ source("R/score.R")
 library(tidyverse)
 
 set.seed(42)
-GRAPH <- FALSE
+GRAPH <- TRUE
 
 df_train_val <- read_csv("Data/train.csv")
 df_test <- read_csv("Data/test.csv")
 
 # Problème avec Nebulosity, non stabilisé pré 2018
-if (GRAPH) plot(df_train_val$Nebulosity)
+plot(df_train_val$Nebulosity)
 names(df_train_val)
 # Stabilisation de Nebulosity
 years <- 2013:2017
@@ -21,10 +21,10 @@ for (year in years) {
   col <- df_train_val$Nebulosity[idx]
   df_train_val$Nebulosity[idx] <- seuil_bas + (col - min(col)) / (max(col) - min(col)) * (seuil_haut - seuil_bas)
 }
-if (GRAPH) plot(df_train_val$Date, df_train_val$Nebulosity)
+plot(df_train_val$Date, df_train_val$Nebulosity)
 
 # Idem pour Nebulosity_weighted
-if (GRAPH) plot(df_train_val$Nebulosity_weighted)
+plot(df_train_val$Nebulosity_weighted)
 seuil_bas <- 65
 seuil_haut <- 96
 for (year in years) {
@@ -32,16 +32,23 @@ for (year in years) {
   col <- df_train_val$Nebulosity_weighted[idx]
   df_train_val$Nebulosity_weighted[idx] <- seuil_bas + (col - min(col)) / (max(col) - min(col)) * (seuil_haut - seuil_bas)
 }
-if (GRAPH) plot(df_train_val$Date, df_train_val$Nebulosity_weighted)
+plot(df_train_val$Date, df_train_val$Nebulosity_weighted)
+
+# On transforme Nebulosity_weighted et Wind_weighted en ratio avec les non weighted
+df_train_val$Nebulosity_weighted <- df_train_val$Nebulosity_weighted / df_train_val$Nebulosity
+df_train_val <- df_train_val %>% rename(Nebulosity_weighted_ratio = Nebulosity_weighted)
+df_train_val$Wind_weighted <- df_train_val$Wind_weighted / df_train_val$Wind
+df_train_val <- df_train_val %>% rename(Wind_weighted_ratio = Wind_weighted)
 
 # On drop "Load","Solar_power","Wind_power" du dataset train, qui ne sont pas présentes dans le dataset test
 df_train_val <- df_train_val %>% select(-Load, -Solar_power, -Wind_power)
 
 # Matrice de corrélation des variables
-# TODO Clarifier la matrice?
 library(corrplot)
 correlation_matrix <- cor(df_train_val %>% select(-Date, -Year, -Month, -Holiday, -Holiday_zone_a, -Holiday_zone_b, -Holiday_zone_c, -toy, -BH_before, -BH, -BH_after, -WeekDays, -DLS, -Summer_break, -Christmas_break, -BH_Holiday) %>% na.omit())
-if (GRAPH) corrplot(correlation_matrix, method = "color", type = "upper", tl.col = "black", tl.srt = 45)
+corrplot(correlation_matrix, method = "color", type = "upper", tl.col = "black", tl.srt = 45)
+
+
 
 # Création du set de validation, on prend la dernière année dans les données d'entraînement
 range(df_train_val$Date)
